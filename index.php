29da57b801cc75a83e349c4d2972e8370d19f556
NIU1638309 <1638309@uab.cat>
2023-10-19 19:39:25 +0200

    up

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página de Pesca</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<header>
+    <img src="view/images/logoCosmar.png" alt="Logo Cosmar" style="float:left; margin-right:10px;">
+    <h1>PESCA COSMAR</h1>
+</header>
+<main>
+<nav>s
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropbtn">Categorías</a>
+            <div class="dropdown-content">
+                <a href="view/categorias/canas_de_pescar.html">Cañas de Pescar</a>
+                <a href="view/categorias/carretes.html">Carretes</a>
+                <a href="view/categorias/anzuelos.html">Anzuelos</a>
+                <a href="view/categorias/accesorios.html">Accesorios</a>
+            </div>
+        </li>
+        <li><a href="#contacto">Contacto</a></li>
+        <li><a href="view/logIn.html"><img src="view/images/iniciar-sesion.png" alt="Iniciar Sesión" style="vertical-align: middle;"></a></li>
+    </ul>
+</nav>
+
+
+
+</main>
+
+<footer>
+    <p>Copyright © 2023, Todos los derechos reservados.</p>
+</footer>
+
+</body>
+</html>
